534cd5db60ce5eafeb78c65eb4eb7d7cc34bd14c
HackeZ <767110505@qq.com>
2016-08-22 16:17:27 +0800

    clear up project

--- src/back-end/controllers/base.go
+++
@@ -1,9 +0,0 @@
-package controllers
-
-func Error(rw http.ResponseWriter, req *http.Request) {
-
-}
-
-func Index(rw http.ResponseWriter, req *http.Request) {
-    
-}
--- src/back-end/controllers/user.go
+++
@@ -1,9 +0,0 @@
-package controllers
-
-func Login(rw http.ResponseWriter, req *http.Request) {
-
-}
-
-func SignUp(rw http.ResponseWriter, req *http.Request) {
-
-}
