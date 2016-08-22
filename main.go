9005421410bc5c180b74a437231579997fc0a6b8
HackeZ <767110505@qq.com>
2016-08-22 16:21:19 +0800

    change web framework

---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	_ "Personal-Dictionary/routers"
+	"github.com/astaxie/beego"
+)
+
+func main() {
+	beego.Run()
+}
+
---
+++ routers/router.go
@@ -0,0 +1,10 @@
+package routers
+
+import (
+	"Personal-Dictionary/controllers"
+	"github.com/astaxie/beego"
+)
+
+func init() {
+    beego.Router("/", &controllers.MainController{})
+}
